655d904d47d92b4264983e8d0b5ed4c9558707c5
Abelardo E. Mendoza <abelardo22.9@gmail.com>
2016-11-24 16:17:42 -0600

    paging fixed

--- frontend/abemar-mikrotik/app/static/scripts/controllers/home.js
+++ frontend/abemar-mikrotik/app/static/scripts/controllers/home.js
@@ -1,24 +1,28 @@
-; (function (angular) {
+; (function(angular) {
     'use strict';
 
     angular.module('app.controllers').controller('HomeCtrl', ['$scope', 'Mikrotik',
-        function ($scope, Mikrotik) {
+        function($scope, Mikrotik) {
             $scope.clients = [];
+            $scope.rowCollection = [];
 
             function requestClients() {
-                Mikrotik.getClients().success(function (response) {
-                    response.forEach(function (element) {
+                Mikrotik.getClients().success(function(response) {
+                    response.forEach(function(element) {
 
                     }, this);
 
-                    $scope.clients = response.map(function (client) {
-                        client.target = client.target.split(',')
+                    $scope.clients = response.map(function(client) {
+                        client.target = client.target.split(',');
+                        $scope.rowCollection.push(client);
+
                         return client;
                     });
+
                 });
             }
 
-            $scope.reloadClients = function () {
+            $scope.reloadClients = function() {
                 requestClients();
             };
 
